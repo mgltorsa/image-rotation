25d5406ec82a97862dc5b489a653309c53e656ff
Miguel Torres <mgltorsa@gmail.com>
2023-05-10 01:29:08 -0400

    updated experiments

--- experiments.sh
+++ experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in {1..2}; do
+for i in {1..10}; do
 
-    mpiexec -n 4 MPIRotateImage james_webb.png output_images 45
-    ./RotateImage james_webb.png output_images/serial_james_webb.png 45
+    mpiexec -n 4 MPIRotateImage hubble_image.png output_images 45
+    ./RotateImage hubble_image.png output_images/serial_hubble_image.png 45
 done
