b762556926a5c512f021b0693000979238c16e0d
Miguel Torres <mgltorsa@gmail.com>
2023-05-10 16:04:33 -0400

    added gcc as require package jic gclib are required

--- experiments.sh
+++ experiments.sh
@@ -6,7 +6,7 @@ vpkg_require gcc
 
 export OPENCV_IO_MAX_IMAGE_PIXELS=1099511627776
 
-for i in {1..5}; do
+for i in {1..10}; do
 
     ./RotateImage hubble_image.png output_images/serial_hubble_image.png 45
     mpiexec -n 4 MPIRotateImage hubble_image.png output_images 45
