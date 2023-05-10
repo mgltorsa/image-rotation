9a038db750a33db5493d97cb7aa8847987a76386
Miguel Torres <mgltorsa@gmail.com>
2023-05-10 14:31:02 -0400

    added compile command and updated experiment command

---
+++ compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+vpkg_require opencv
+vpkg_require openmpi
+vpkg_require gcc
+vpkg_require cmake
--- experiments.sh
+++ experiments.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# vpkg_require opencv
-# vpkg_require openmpi
+vpkg_require opencv
+vpkg_require openmpi
+vpkg_require gcc
+
 export OPENCV_IO_MAX_IMAGE_PIXELS=1099511627776
 
 for i in {1..5}; do
